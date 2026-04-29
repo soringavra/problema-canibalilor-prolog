4c16c6f6d1c63d1187b613afd013f7940bbff130
soringavra <soringvr2@gmail.com>
2026-04-29 12:36:56 +0300

    Update solution

--- main.pl
+++ main.pl
@@ -1,49 +1,33 @@
-:- initialization(solve, main).
-% stare(NCanibaliStg, NCanibaliDr, NMisionariStg, NMisionariDr, PozitieBarca)
-% Starea initiala: toti pe stanga, barca pe stanga
-stare_initiala(stare(3, 0, 3, 0, stg)).
+stare_initiala(stare(0, 3, 0, 3, dr)).
 
-% Starea finala: toti pe dreapta
-stare_finala(stare(0, 3, 0, 3, dr)).
+stare_finala(stare(3, 0, 3, 0, stg)).
 
-% Validarea starii - nimeni nu e in pericol
+% Validarea starilor
 in_siguranta(stare(CS, CD, MS, MD, _)) :-
-    % Stanga: daca exista misionari, sa fie >= canibali
     (MS > 0 -> MS >= CS ; true),
-    % Dreapta: daca exista misionari, sa fie >= canibali
     (MD > 0 -> MD >= CD ; true),
-    % Valori nenegative
     CS >= 0, CD >= 0, MS >= 0, MD >= 0.
 
-% Mutari posibile (1-2 persoane in barca)
-% (DeltaCanibali, DeltaMisionari)
+% Mutarile posibile si aplicarea lor
 mutare(1, 0).
 mutare(2, 0).
 mutare(0, 1).
 mutare(0, 2).
 mutare(1, 1).
 
-% Aplica mutarea si verifica starea rezultata
-traverseaza(stare(CS, CD, MS, MD, stg), stare(CS2, CD2, MS2, MD2, dr), Mutare) :-
+traverseaza(stare(CS, CD, MS, MD, stg), stare(CS2, CD2, MS2, MD2, dr), mutare(DC, DM)) :-
     mutare(DC, DM),
-    Mutare = mutare(DC, DM),
-    CS2 is CS - DC,
-    CD2 is CD + DC,
-    MS2 is MS - DM,
-    MD2 is MD + DM,
+    CS2 is CS - DC, CD2 is CD + DC,
+    MS2 is MS - DM, MD2 is MD + DM,
     in_siguranta(stare(CS2, CD2, MS2, MD2, dr)).
 
-traverseaza(stare(CS, CD, MS, MD, dr), stare(CS2, CD2, MS2, MD2, stg), Mutare) :-
+traverseaza(stare(CS, CD, MS, MD, dr), stare(CS2, CD2, MS2, MD2, stg), mutare(DC, DM)) :-
     mutare(DC, DM),
-    Mutare = mutare(DC, DM),
-    CS2 is CS + DC,
-    CD2 is CD - DC,
-    MS2 is MS + DM,
-    MD2 is MD - DM,
+    CS2 is CS + DC, CD2 is CD - DC,
+    MS2 is MS + DM, MD2 is MD - DM,
     in_siguranta(stare(CS2, CD2, MS2, MD2, stg)).
 
-% DFS cu evitarea ciclurilor
-% traseu(StareaCurenta, StariVizitate, Pasi)
+% Algoritmul traseului - DFS si evitarea ciclurilor
 traseu(StareFinala, _, []) :-
     stare_finala(StareFinala).
 
@@ -52,31 +36,29 @@ traseu(StareCurenta, Vizitate, [Mutare | RestPasi]) :-
     \+ member(StareUrmatoare, Vizitate),
     traseu(StareUrmatoare, [StareUrmatoare | Vizitate], RestPasi).
 
-% Afisarea rezultatului
+% Afisare
 afiseaza_pas(mutare(DC, DM), PozBarca) :-
     Total is DC + DM,
     (PozBarca = stg ->
-        format("  Trec de pe stanga pe dreapta: ~w canibal(i), ~w misionar(i) [~w persoana(e)]~n",
-               [DC, DM, Total]);
-        format("  Trec de pe dreapta pe stanga: ~w canibal(i), ~w misionar(i) [~w persoana(e)]~n",
-               [DC, DM, Total])
+        format("  Trec stg->dr: ~w canibal(i), ~w misionar(i) [~w pers.]~n", [DC, DM, Total])
+    ;
+        format("  Trec dr->stg: ~w canibal(i), ~w misionar(i) [~w pers.]~n", [DC, DM, Total])
     ).
 
 afiseaza_solutie(_, []).
 afiseaza_solutie(stare(CS, CD, MS, MD, Pos), [Mutare | Rest]) :-
-    format("Stare: stg(~w C, ~w M) | barca ~w | dr(~w C, ~w M)~n",
-           [CS, MS, Pos, CD, MD]),
+    format("Stare: stg(~w C, ~w M) | barca ~w | dr(~w C, ~w M)~n", [CS, MS, Pos, CD, MD]),
     afiseaza_pas(Mutare, Pos),
-    traverseaza(stare(CS, CD, MS, MD, Pos), StareUrm, Mutare),
+    once(traverseaza(stare(CS, CD, MS, MD, Pos), StareUrm, Mutare)),
     afiseaza_solutie(StareUrm, Rest).
 
-% Predicatul principal
-solve :-
+% Main
+main :-
     stare_initiala(StareInit),
-    traseu(StareInit, [StareInit], Pasi),
+    once(traseu(StareInit, [StareInit], Pasi)),
     length(Pasi, N),
     format("~nSolutie gasita in ~w pasi:~n~n", [N]),
     afiseaza_solutie(StareInit, Pasi),
-    stare_finala(StareFinala),
-    format("Stare: ~w~n", [StareFinala]),
-    format("~nToti au trecut in siguranta!~n").
+    stare_finala(stare(CS, CD, MS, MD, Pos)),
+    format("Stare: stg(~w C, ~w M) | barca ~w | dr(~w C, ~w M)~n", [CS, MS, Pos, CD, MD]),
+    format("~nToti au trecut in siguranta pe malul stang!~n").
